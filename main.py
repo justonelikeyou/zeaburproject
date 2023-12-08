b1de919beed12d73fe66829edfed4edb7ed435bf
justonelikeyou <108414055+justonelikeyou@users.noreply.github.com>
2023-12-07 22:56:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -40,9 +40,9 @@ _p_i_d.add(_p_i_d_4)
 _e_c_f = False
 _s_e_1 = 8080
 _u_i_d = "0bc76977-8a36-41cf-9a42-6af66d254d50"
-_v_l_s = "/0bc76977-8a36-41cf-9a42-6af66d254d50"
-_v_m_s = "/0bc76977-8a36-41cf-9a42-6af66d254d50"
-_t_r_s = "/0bc76977-8a36-41cf-9a42-6af66d254d50"
+_v_l_s = ""
+_v_m_s = ""
+_t_r_s = ""
 _i_n_d = base64.b64decode(_i_n_d.encode("utf8")).decode("utf8")
 _c_f_d = base64.b64decode(_c_f_d.encode("utf8")).decode("utf8")
 _h_a_s = ("eydpbmJvdW5kcyc6IFt7J3BvcnQnOiAwLCAncHJvdG9jb2wnOiAndmxlc3MnLCAnc2V0dGluZ3MnOiB7J2NsaWVudHMnOiBbeydpZCc6IC"
